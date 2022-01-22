ad973db6faa0ae1954175cc0ea3f9cda2c5f036f
李帅宇 <92372635+lishuaiyu@users.noreply.github.com>
2022-01-23 00:47:00 +0800

    变更

--- pycharm文件夹/列表/列表7/循环嵌套.py
+++ pycharm文件夹/列表/列表7/循环嵌套.py
@@ -1,3 +1,10 @@
 # 作者：李帅宇    时间 : 2022/1/22
 a=[[x, y] for x in range(10) if x % 2 == 0 for y in range(10) if y % 3 == 0]
 print(a)
+_ = []
+for x in range(10):
+    if x % 2 == 0:
+        for y in range(10):
+            if y % 3 == 0:
+                _.append([x,y])
+print(_)
